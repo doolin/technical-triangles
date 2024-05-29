8f056c9f9037cc8a0df7aae370225d5cf7b6682e
dave doolin <david.doolin@gmail.com>
2024-05-29 04:27:05 -0700

    more linting and refactoring

--- arrowhead.rb
+++ arrowhead.rb
@@ -10,8 +10,8 @@ class Arrowhead
     @xml = xml
   end
 
-  def build
-    xml.marker(
+  def marker
+    {
       id: 'arrowhead',
       viewBox: '0 0 80 80',
       refX: '35',
@@ -21,7 +21,11 @@ class Arrowhead
       markerHeight: '11',
       markerUnits: 'userSpaceOnUse',
       orient: 'auto-start-reverse'
-    ) do
+    }
+  end
+
+  def build
+    xml.marker(marker) do
       xml.path(d: 'M 5,25 L 0,40 L 40,25 L 0,10 Z')
     end
   end
@@ -29,12 +33,11 @@ class Arrowhead
   def self.write(options = {})
     filename = options[:outfile] || '/tmp/arrowhead.svg'
 
-    File.open(filename, 'w') do |f|
-      f.write(Nokogiri::XML::Builder.new do |xml|
+    File.write(filename,
+      Nokogiri::XML::Builder.new do |xml|
         xml.svg do
           Arrowhead.new(xml).build
         end
       end.to_xml)
-    end
   end
 end
--- bin/arrowhead.rb
+++ bin/arrowhead.rb
@@ -1,18 +1,8 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'nokogiri'
 require_relative '../arrowhead'
-
-# outfile = './banner.svg'
-# options = { outfile: './banner.svg' }
-
-# banner = Nokogiri::XML::Builder.new do |xml|
-#   xml.svg do
-#     Banner.new(xml, 0, 0, options).draw
-#   end
-# end
-
-# File.open(outfile, 'w') { |f| f.write(banner.to_xml) }
 
 options = { outfile: '/tmp/arrowhead.svg' }
 Arrowhead.write(options)
