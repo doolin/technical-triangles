ce5a19d9453a83ee79a12281094283bef62f8b90
dave doolin <david.doolin@gmail.com>
2024-06-03 04:16:50 -0700

    gradient refactoring

--- gradient.rb
+++ gradient.rb
@@ -14,39 +14,51 @@ class Gradient
     @gradient_center = gradient_center
   end
 
-  def build
+  def attrs
+    {
+      id: 'vertexGradient',
+      cx: @gradient_center[:x],
+      cy: @gradient_center[:y],
+      r: '100',
+      fx: @gradient_center[:x],
+      fy: @gradient_center[:y]
+    }
+  end
+
+  def build(xml)
+    xml.radialGradient(attrs) do
+      @gradient_colors.each do |offset, color|
+        xml.stop(offset:, 'stop-color' => color)
+      end
+    end
   end
 
   def generate_svg
     result = Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
-      xml.svg(xmlns: 'http://www.w3.org/2000/svg', width: @width, height: @height) {
-        xml.defs {
-          xml.radialGradient(id: 'vertexGradient', cx: @gradient_center[:x], cy: @gradient_center[:y], r: '100', fx: @gradient_center[:x], fy: @gradient_center[:y]) {
-            @gradient_colors.each do |offset, color|
-              xml.stop(offset: offset, 'stop-color' => color)
-            end
-          }
-        }
+      xml.svg(xmlns: 'http://www.w3.org/2000/svg', width: @width, height: @height) do
+        xml.defs do
+          build(xml)
+        end
         xml.polygon(points: @vertices.map { |v| "#{v[:x]},#{v[:y]}" }.join(' '), fill: 'url(#vertexGradient)')
-      }
+      end
     end.to_xml
-    File.write('gradient.svg', result)
+    File.write('./images/gradient.svg', result)
   end
 end
 
 # Define triangle vertices and gradient details
 vertices = [{x: 50, y: 50}, {x: 150, y: 50}, {x: 100, y: 150}]
 # gradient_colors = {'0%' => '#ff0000', '100%' => '#0000ff'}
-gradient_colors = {'40%' => '#ff0000', '50%' => '#0000ff'}
+gradient_colors = {'30%' => '#ff0450', '20%' => '#ff00ff'}
 gradient_center = {x: 50, y: 50}
 
 # Create an instance of the Gradient class
 svg_creator = Gradient.new(
   width: 200,
   height: 200,
-  vertices: vertices,
-  gradient_colors: gradient_colors,
-  gradient_center: gradient_center
+  vertices:,
+  gradient_colors:,
+  gradient_center:
 )
 
 # Generate SVG
