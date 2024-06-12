8b53c35e3a080a9008a71de4e310998afc575369
dave doolin <david.doolin@gmail.com>
2024-06-12 09:07:38 -0700

    removed executable from generator

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,4 +1,3 @@
-#!/usr/bin/env ruby
 # frozen_string_literal: true
 
 require 'nokogiri'
@@ -146,7 +145,6 @@ class Generator
 
     # This is the bottom arrowhead
     y1 = y + ARROW_GAP
-
     # Top arrowhead
     y2 = (BASELINE * Math.tan(r2d(angle))) - ARROW_GAP
 
@@ -335,50 +333,3 @@ class Generator
     end
   end
 end
-
-options = {
-  ykyk_fill: 'lightgreen',
-  ykydk_fill: 'lightblue',
-  ydkydk_fill: 'pink',
-  angle: 45,
-  first_line_offset: 0.125,
-  second_line_offset: 0.28
-}
-result = Generator.new(options).build do |xml, scale, banner_y, ykyk_fill|
-  Banner.new(xml, scale + 10, banner_y, fill: ykyk_fill, stroke: 'green', depth: 3).draw
-  Banner.new(xml, scale - 18, banner_y, fill: ykyk_fill, stroke: 'green', depth: 12).draw
-  Banner.new(xml, scale - 10, banner_y, fill: ykyk_fill, stroke: 'green', depth: 19, width: 6).draw
-end
-File.write('images/sswe.svg', result.to_xml)
-
-options = {
-  ykyk_fill: 'lightgreen',
-  ykydk_fill: 'lightblue',
-  ydkydk_fill: 'pink',
-  angle: 55,
-  first_line_offset: 0.09,
-  second_line_offset: 0.14
-}
-result = Generator.new(options).build do |xml, scale, banner_y, ykyk_fill|
-  Banner.new(xml, scale + 10, banner_y, fill: ykyk_fill, stroke: 'green', depth: 3).draw
-  Banner.new(xml, scale - 19, banner_y, fill: ykyk_fill, stroke: 'green', depth: 2).draw
-  # Banner.new(xml, scale - 10, banner_y, fill: ykyk_fill, stroke: 'green', depth: 19, width: 6).draw
-end
-File.write('images/jswe.svg', result.to_xml)
-
-options = {
-  ykyk_fill: 'lightgreen',
-  ykydk_fill: 'lightblue',
-  ydkydk_fill: 'pink',
-  angle: 35,
-  first_line_offset: 0.09,
-  second_line_offset: 0.32
-}
-result = Generator.new(options).build do |xml, scale, banner_y, ykyk_fill|
-  Banner.new(xml, scale + 10, banner_y, fill: ykyk_fill, stroke: 'green', depth: 3).draw
-  Banner.new(xml, scale + 19, banner_y, fill: ykyk_fill, stroke: 'green', depth: 2, width: 2).draw
-  Banner.new(xml, scale - 19, banner_y, fill: ykyk_fill, stroke: 'green', depth: 2).draw
-  Banner.new(xml, scale - 10, banner_y, fill: ykyk_fill, stroke: 'green', depth: 11, width: 2).draw
-  Banner.new(xml, scale - 7, banner_y, fill: ykyk_fill, stroke: 'green', depth: 9, width: 2).draw
-end
-File.write('images/pswe.svg', result.to_xml)
